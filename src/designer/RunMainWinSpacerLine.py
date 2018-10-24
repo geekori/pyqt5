e87aa0760e62b552a49bac5db573adfe639dd88a
lining <helloworld@126.com>
2018-10-24 08:21:45 +0800

    SpacerLine

---
+++ src/designer/RunMainWinSpacerLine.py
@@ -0,0 +1,12 @@
+import sys
+import MainWinSpacerLine
+from PyQt5.QtWidgets import QApplication,QMainWindow
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    mainWindow = QMainWindow()
+    ui = MainWinSpacerLine.Ui_MainWindow()
+    # 向主窗口上添加控件
+    ui.setupUi(mainWindow)
+    mainWindow.show()
+    sys.exit(app.exec_())
