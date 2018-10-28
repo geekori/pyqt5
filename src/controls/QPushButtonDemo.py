028e65311f0fb46bdba045a2762f444f4aab3ead
lining <helloworld@126.com>
2018-10-28 19:59:55 +0800

    QPushButton

---
+++ src/controls/QPushButtonDemo.py
@@ -0,0 +1,73 @@
+'''
+
+按钮控件（QPushButton）
+
+QAbstractButton
+
+QPushButton
+AToolButton
+QRadioButton
+QCheckBox
+
+
+'''
+
+
+import sys
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+
+class QPushButtonDemo(QDialog) :
+    def __init__(self):
+        super(QPushButtonDemo,self).__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.setWindowTitle('QPushButton Demo')
+
+        layout = QVBoxLayout()
+
+        self.button1 = QPushButton('第1个按钮')
+        self.button1.setText('First Button1')
+        self.button1.setCheckable(True)
+        self.button1.toggle()
+        self.button1.clicked.connect(self.buttonState)
+        self.button1.clicked.connect(lambda:self.whichButton(self.button1))
+
+        layout.addWidget(self.button1)
+
+        # 在文本前面显示图像
+
+        self.button2 = QPushButton('图像按钮')
+        self.button2.setIcon(QIcon(QPixmap('./images/python.png')))
+        self.button2.clicked.connect(lambda:self.whichButton(self.button2))
+        layout.addWidget(self.button2)
+
+        self.button3 = QPushButton('不可用的按钮')
+        self.button3.setEnabled(False)
+        layout.addWidget(self.button3)
+
+        self.button4 = QPushButton('&MyButton')
+        self.button4.setDefault(True)
+        self.button4.clicked.connect(lambda:self.whichButton(self.button4))
+        layout.addWidget(self.button4)
+
+        self.setLayout(layout)
+        self.resize(400,300)
+
+    def buttonState(self):
+        if self.button1.isChecked():
+            print('按钮1已经被选中')
+        else:
+            print('按钮1未被选中')
+
+    def whichButton(self,btn):
+        print('被单击的按钮是<' + btn.text() + '>')
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    main = QPushButtonDemo()
+    main.show()
+    sys.exit(app.exec_())
